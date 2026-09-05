854476c7815e0ec92fc264a48f6eee3aa13b1a10
ToxicCodelife <roguebush@gmail.com>
2026-09-05 08:18:38 +0000

    registered environmental secrets securely inside sails configuration mapping

--- server/config/custom.js
+++ server/config/custom.js
@@ -32,6 +32,9 @@ const baseUrl = envToArray(process.env.BASE_URL)[0] || 'http://localhost:1337';
 const parsedBasedUrl = new URL(baseUrl);
 
 module.exports.custom = {
+  twitchClientId: process.env.TWITCH_CLIENT_ID,
+  twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
+
   /**
    *
    * Any other custom config this Sails app should use during development.
