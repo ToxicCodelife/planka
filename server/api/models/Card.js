98fb3a804e63f6500f8d88083097940fb7191002
ToxicCodelife <roguebush@gmail.com>
2026-09-05 20:05:31 +0000

    bot-assisted fix for 502 gateway boot crash

--- server/api/models/Card.js
+++ server/api/models/Card.js
@@ -1,15 +1,19 @@
+const Types = {
+  PROJECT: 'project',
+  STORY: 'story',
+};
+
 module.exports = {
+  Types,
+
   attributes: {
-    list: {
-      model: 'list',
-      required: true,
-    },
-    creatorUser: {
-      model: 'user',
-      required: true,
-    },
-    coverAttachmentId: {
+    type: {
       type: 'string',
+      isIn: Object.values(Types),
+      required: true,
+    },
+    position: {
+      type: 'number',
       allowNull: true,
     },
     name: {
@@ -18,23 +22,99 @@ module.exports = {
     },
     description: {
       type: 'string',
+      isNotEmptyString: true,
       allowNull: true,
     },
+    dueDate: {
+      type: 'ref',
+      columnName: 'due_date',
+    },
+    isDueCompleted: {
+      type: 'boolean',
+      allowNull: true,
+      columnName: 'is_due_completed',
+    },
+    stopwatch: {
+      type: 'json',
+    },
+    commentsTotal: {
+      type: 'number',
+      defaultsTo: 0,
+      columnName: 'comments_total',
+    },
+    isClosed: {
+      type: 'boolean',
+      defaultsTo: false,
+      columnName: 'is_closed',
+    },
+    listChangedAt: {
+      type: 'ref',
+      columnName: 'list_changed_at',
+    },
+    boardId: {
+      model: 'Board',
+      required: true,
+      columnName: 'board_id',
+    },
+    listId: {
+      model: 'List',
+      required: true,
+      columnName: 'list_id',
+    },
+    creatorUserId: {
+      model: 'User',
+      columnName: 'creator_user_id',
+    },
+    prevListId: {
+      model: 'List',
+      columnName: 'prev_list_id',
+    },
+    coverAttachmentId: {
+      model: 'Attachment',
+      columnName: 'cover_attachment_id',
+    },
+    subscriptionUsers: {
+      collection: 'User',
+      via: 'cardId',
+      through: 'CardSubscription',
+    },
+    memberUsers: {
+      collection: 'User',
+      via: 'cardId',
+      through: 'CardMembership',
+    },
+    labels: {
+      collection: 'Label',
+      via: 'cardId',
+      through: 'CardLabel',
+    },
+    taskLists: {
+      collection: 'TaskList',
+      via: 'cardId',
+    },
+    attachments: {
+      collection: 'Attachment',
+      via: 'cardId',
+    },
+    comments: {
+      collection: 'Comment',
+      via: 'cardId',
+    },
     actions: {
-      collection: 'action',
+      collection: 'Action',
       via: 'cardId',
     },
   },
 
   afterCreate(newlyCreatedRecord, proceed) {
     Card.findOne({ id: newlyCreatedRecord.id })
-      .populate('list')
+      .populate('listId')
       .then((card) => {
-        if (!card || !card.list) {
+        if (!card || !card.listId) {
           return proceed();
         }
 
-        return Board.findOne({ id: card.list.boardId }).then((board) => {
+        return Board.findOne({ id: card.listId.boardId }).then((board) => {
           if (!board) {
             return proceed();
           }
@@ -49,7 +129,7 @@ module.exports = {
                 card,
                 project,
                 board,
-                list: card.list,
+                list: card.listId,
                 creatorUser: newlyCreatedRecord.creatorUserId || 1,
               })
               .exec((err) => {
--- server/config/env/production.js
+++ server/config/env/production.js
@@ -23,7 +23,9 @@ const { URL } = require('url');
 
 const { customLogger } = require('../../utils/logger');
 
-const origins = process.env.BASE_URL.split(',').map((baseUrl) => new URL(baseUrl).origin);
+const origins = (process.env.BASE_URL || 'http://localhost:1337')
+  .split(',')
+  .map((baseUrl) => new URL(baseUrl).origin);
 
 module.exports = {
   /**
